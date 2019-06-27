e2666c6ba62ede36cc256521c26e33bb09f85cf8
Mike <mcandersonmusic@gmail.com>
2019-06-27 08:09:16 -0500

    Uploaded Project Files

---
+++ main.py
@@ -0,0 +1,62 @@
+import os
+import csv
+
+#ask about this filepath
+csvpath = os.path.join("budget_data.csv")
+
+#defines the lists
+months = []
+profits_losses = []
+changes = []
+
+#opens the csv file and reads it
+with open(csvpath, newline='', encoding='utf8') as budget_data:
+    csvreader = csv.reader(budget_data, delimiter=",")
+    csv_header = next(csvreader)
+
+    #populates the lists
+    for row in csvreader:
+        months.append(row[0])
+        profits_losses.append(int(row[1]))
+
+    total = sum(profits_losses)
+
+    #calculates the change from one month to the next
+    for prof_loss in profits_losses:
+        if prof_loss == profits_losses[0]:
+            previous = prof_loss
+        else:
+            change = prof_loss - previous
+            changes.append(change)
+            previous = prof_loss
+
+    #assigns variables for printing
+    average = sum(changes) / (len(months)-1)
+    greatest_increase_index = changes.index(max(changes))
+    greatest_decrease_index = changes.index(min(changes))
+
+#output
+print(f"""
+-----------------
+Financial Data
+-----------------
+Total Number of Months: {len(months)}
+Total Profits/Losses: ${total}
+Average Change: ${round(average,2)}
+Greatest Increase: {months[greatest_increase_index + 1]} (${changes[greatest_increase_index]})
+Greatest Decrease: {months[greatest_decrease_index +1]} (${changes[greatest_decrease_index]})
+""")
+
+output_file = os.path.join("PyBank_output.txt")
+
+output = open(output_file, "w")
+output.write(f"""
+-----------------
+Financial Data
+-----------------
+Total Number of Months: {len(months)}
+Total Profits/Losses: ${total}
+Average Change: ${round(average,2)}
+Greatest Increase: {months[greatest_increase_index + 1]} (${changes[greatest_increase_index]})
+Greatest Decrease: {months[greatest_decrease_index +1]} (${changes[greatest_decrease_index]})
+""")
